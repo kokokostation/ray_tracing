86251d46462986921325db6eae9a22c4faa5e5d0
Aleksandr Manakov <kokokostation@gmail.com>
2016-12-04 21:28:38 +0300

    qt strange behaiviour got round

--- main.cpp
+++ main.cpp
@@ -8,10 +8,13 @@ int main(int argc, char *argv[])
 {
     std::fstream in("input.rt", std::ios_base::in);
 
-    QApplication a(argc, argv);
-    ray_tracing::Main_window w(ray_tracing::Tracer(ray_tracing::parse(in)).produce_picture());
+    ray_tracing::Matrix result(ray_tracing::Tracer(ray_tracing::parse(in)).produce_picture());
 
     in.close();
+
+    QApplication a(argc, argv);
+
+    ray_tracing::Main_window w(result);
 
     w.show();
 
